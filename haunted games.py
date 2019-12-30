1a22215187e40f2ff69f4e0bbea49e4ffa5c8e91
Neerraj <neerrajonline@gmail.com>
2019-12-30 12:43:24 -0500

    Haunted stories v1

---
+++ haunted games.py
@@ -0,0 +1,81 @@
+#Haunted games
+from time import *
+ch1=False
+
+print ("welcome to this choice story game, enjoy! oh and BEWARE [press ENTER to continue]")
+input()
+print (" 'i'm bored,' Sania says, you had gone to your friend Sunil's house to play on his new XBox 4,\n but the electricity depatment had other plans")
+input()
+print("'dont you have any board games Sunil?' you ask, Kshithi, Sania, Sukuraman nodded in agreement")
+input()
+print("'uhhh... no'")
+input()
+print("'Liar! I saw some boardgame in the cupboard downstairs', Kshithi folded her arms and spuinted at Sunil")
+input()
+print("'Look, I'm not allowed to touch it, its my grandmom's'")
+input()
+print("'It's a boardgame you idiot")
+sleep(1)
+print("ugh fine Mr.Goody two shoes' kshithi sighed,'bro close the door'")
+input()
+print("'ok' you say, you go to close the door but when you see something outside,'uhhh guys? look what I found'")
+input()
+print("Suku comes to your side, 'trigonomi, never heard of it' he says")
+input()
+print("you set the game on the bed\n'uhh guys I don't feel good about this game, and I also think it's from my grandma's cupboard'")
+input()
+print("'oh go boil your head' Sania says and you open the lid")
+input()
+print("the board part of the game was painted gold (or that's what you thought), \nand was shaped like a five pointed star with funky shapes decorating the border and the insides of the arms of the stars")
+input()
+print("there were five wood carved pawns and a candle")
+input()
+print("'I guess the candle goes in the center' Sania guesses 'light it up' ")
+input()
+print("you place the candle in the middle and and Kshithi lights it up")
+input()
+print("'I think the pawns go on the arm of the sta...' you stop, it feels like something was sucked out of you")
+input()
+print("and judging by the expression on your friends you can say you weren't the only one who felt it")
+c1=input("type continue to play or type stop: ")
+if c1 == 'countinue to play' :
+    ch1 = True
+    while ch1 == True: 
+       ch1 = False
+       break
+    
+    
+if c1== 'stop' :
+    print("Something's wrong, andd you know everyone can feel it, it's best if you stopped now before something happens")
+    input()
+    print("'guys', I think we shouldn't play this game somethings doesn't feel right' you say")
+    input()
+    print("'ya he's right' Kshithi says")
+    input()
+    print("'Someone changed their mind'")
+    input()
+    print("'Shut up'")
+    input()
+    print("You ignore the squabble between Kshithi and Sania and open the door, or atleast you try to")
+    input()
+    print("'guys...GUYS! the doors not opening' you say")
+    input()
+    print("all of your friends try the door handle its been locked")
+    input()
+    print("it's okay its not a ghost, they don't exist")
+    input()
+    print("For a momment everyone is quiet you look at each other")
+    input()
+    print("'It's a ghost isn't it'")
+    input()
+    print("and just then Sania screams")
+    input()
+    print("you flip around and see someone scratches on the wall, you were okay until the scratches became legible")
+    input()
+    print("FINISH THE GAME, OR THE GAME WILL FINISH YOU!")
+    ch1 = True
+    while ch1 == True:
+        break
+
+print ("Chapter 2 coming soon !")
+    
